1fb3a8bdcc0c7590668ece7afbb84b1ec44d05cb
Rajat Verma <rajat.verma@devslane.com>
2021-10-13 16:30:07 +0530

    tailwind setup

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -4,6 +4,10 @@ interface OwnProps {}
 
 type Props = OwnProps;
 
-const App: FunctionComponent<Props> = () => <h1>Like a Local- Dashboard</h1>;
+const App: FunctionComponent<Props> = () => (
+  <div className='m-auto antialiased font-sans font-serif font-mono text-center'>
+    <h1 className='text-indigo-700'>Hello</h1>
+  </div>
+);
 
 export default App;
