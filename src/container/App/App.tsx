97ed807fa879067ea38294c7cbafb9b9ebe81c85
Rajat Verma <rajat.verma@devslane.com>
2021-10-13 15:06:32 +0530

    vite initial setup

---
+++ src/container/App/App.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react';
+
+interface OwnProps {}
+
+type Props = OwnProps;
+
+const App: FunctionComponent<Props> = (props) => {
+
+  return (<h1>Like a Local- Dashboard</h1>);
+};
+
+export default App;
